931de28fc70f7bf2035579f08ea3b4897f73a0b8
Kim Ly Buck <kim.3993@gmail.com>
2020-04-18 21:06:53 -0500

    create useDarkMode

---
+++ src/hooks/useDarkMode.js
@@ -0,0 +1,14 @@
+import useLocalStorage from './useLocalStorage';
+import useEffect from 'react';
+
+export default function useDarkMode(key) {
+	const [ darkMode, setDarkMode ] = useLocalStorage(key, false);
+	useEffect(
+		() => {
+			document.body.classList.toggle('dark-mode', darkMode);
+		},
+		[ darkMode ]
+	);
+
+	return [ darkMode, setDarkMode ];
+}
